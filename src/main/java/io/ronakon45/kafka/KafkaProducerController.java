cb5df0160c2f71eda46010f5235774c8b4bf0a78
RONAKON <ronakon.wat@axonstech.com>
2023-05-29 14:36:49 +0700

    Upgrade Post/Messages return offset

--- src/main/java/io/ronakon45/kafka/KafkaProducerController.java
+++ src/main/java/io/ronakon45/kafka/KafkaProducerController.java
@@ -1,6 +1,11 @@
 package io.ronakon45.kafka;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +19,20 @@ public class KafkaProducerController {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @PostMapping("/messages")
-    public String sendMessage(@RequestBody String message) {
-        kafkaTemplate.send(TOPIC_NAME, message);
-        return "Message sent: " + message;
+    public CompletableFuture<String> sendMessage(@RequestBody String message) {
+       AtomicReference<String> msg = new AtomicReference<>("a");
+       CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(TOPIC_NAME, message);
+    
+       return future.thenApply(result -> {
+           msg.set("Sent message=[" + message +
+           "] with offset=[" + result.getRecordMetadata().offset() + "]"); // Update the value of msg
+           System.out.println("Sent message=[" + message +
+                   "] with offset=[" + result.getRecordMetadata().offset() + "]");
+           return msg.get();
+       });
     }
+    
+    
+    
+    
 }
